b294c8b47bb835c622cdcc82b6cde0cca181f1f6
David Stein <davidstein@Davids-MacBook-Pro.local>
2017-06-04 15:38:06 -0600

    Added projects page.

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageMessage: "Check out my projects!"
+    })
+});
+
 // /bad send back json with error message
 app.get('/bad', (req, res) => {
     res.send({
